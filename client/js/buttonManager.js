68b14c2cbeea2a7043986239f8322b188caf6367
hecker011 <flebedev77@outlook.com>
2024-01-23 14:36:17 +1030

    Created the button manager, which removes clutter from navbar

--- client/js/buttonManager.js
+++ client/js/buttonManager.js
@@ -1,9 +1,12 @@
+//this script is responsible for hiding unessesary buttons
+// eg Dont show the login button when logged in
+
 async function authenticate(hash) {
     return new Promise((resolve, reject) => {
         const data = {
             cookie: hash
         }
-        fetch("/authenticate", {
+        fetch("http://" + window.location.host + "/authenticate", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -19,12 +22,25 @@ async function authenticate(hash) {
 }
 
 window.onload = function () {
+    //returns a list of [Sign up button, My account button, Sign out button]
+    const Signup = document.getElementsByClassName("signup-button");
+
+    Signup[1].style.display = "none"
+    Signup[2].style.display = "none"
+
     authenticate(getCookie("remember")).then(function (value) {
         if (value == true) {
             console.log("Account logged in");
 
             const Login = document.querySelector(".login-button");
             Login.style.display = "none";
+
+            //get the sign up button from the list
+            Signup[0].style.display = "none";
+
+
+            Signup[1].style.display = ""
+            Signup[2].style.display = ""
         }
     })
 }
