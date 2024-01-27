26250cf1931108f7eb742be9082b6caf602afd1e
flebedev77 <flebedev99@gmail.com>
2024-01-27 16:31:58 +1030

    Updated the run script

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
+@echo off
+title Ecommerce-shop-server By flebedev77
+
+echo "---------------------"
+echo "|   by flebedev77   |"
+echo "---------------------"
+echo Booting up the server... (CTRL+C to exit)
+
 :forever
 node server.js
 goto forever
